2893a73910c32da48d56d38d5a90addc0f721210
reganxzheng <frankiexzjwh@gmail.com>
2019-05-16 13:13:20 +0800

    final version of guessing game.

--- app/src/main/java/nz/ac/aut/dms/android/GameActivity.java
+++ app/src/main/java/nz/ac/aut/dms/android/GameActivity.java
@@ -6,42 +6,69 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.Random;
 
 
-public class GameActivity extends Activity {
-    private EditText guess;
-    private int score;
-    private TextView scoreText, messageText;
-    private int randNum;
+public class GameActivity extends Activity implements View.OnClickListener {
+    public static final int MAX_NUMBER = 100;
+    public static final Random rand = new Random();
+    private TextView textmsg;
+    private EditText numberInput;
+    private Button validate;
+    private int numberToFind, tryCounts, scores = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
-        messageText = (TextView) findViewById(R.id.textMessage);
-        guess = (EditText) findViewById(R.id.numGuess);
-
+        textmsg = (TextView) findViewById(R.id.textMessage);
+        numberInput = (EditText) findViewById(R.id.numGuess);
+        validate = (Button)findViewById(R.id.guessBtn);
+        validate.setOnClickListener(this);
+        newGame();
     }
 
-    private void generateNumbers() {
-        Random rand = new Random();
-        randNum = rand.nextInt(100);
-    }
-
-    private String check() {
-        int number = Integer.parseInt(String.valueOf(guess.getText()));
-        String notify = "Correct!";
-        if(randNum>number) {
-            notify = "Guess too low, try again";
+    @Override
+    public void onClick(View v) {
+        if(v==validate) {
+            validate();
         }
-        else if(randNum<number) {
-            notify = "Guess too high, try again";
-        }
-        return notify;
     }
 
-   public void btnClicked(View view) {
-        messageText.setText(this.check());
-   }
+    private void validate() {
+        int n = Integer.parseInt(numberInput.getText().toString());
+        tryCounts++;
+
+        if(n==numberToFind) {
+            if(tryCounts<=5) {
+                scores+=10;
+            } else if (tryCounts>5&&tryCounts<=10) {
+                scores+=5;
+            } else {
+                scores-=5;
+            }
+
+            textmsg.setText("Correct! You have find the number " + numberToFind + " in " + tryCounts + " tries\nScores: " +
+                    scores+ "\nPlease start a new game!");
+            newGame();
+        }
+        else if (n>numberToFind) {
+            textmsg.setText("Too hight, try again! Count: " + tryCounts
+            + "\nScores: " + scores);
+        }
+        else if (n<numberToFind) {
+            textmsg.setText("Too low, try again! Count: " +tryCounts
+            + "\nScores: " + scores);
+        }
+    }
+
+    private void newGame() {
+        numberToFind = rand.nextInt(MAX_NUMBER)+1;
+        numberInput.setText("");
+        tryCounts=0;
+    }
 }
