14bf4c6c68fa65af98694578e55f61661a36c390
reganxzheng <frankiexzjwh@gmail.com>
2019-05-16 16:49:13 +1200

    new game_layout

--- app/src/main/java/nz/ac/aut/dms/android/GameActivity.java
+++ app/src/main/java/nz/ac/aut/dms/android/GameActivity.java
@@ -1,6 +1,47 @@
 package nz.ac.aut.dms.android;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
 
 public class GameActivity extends Activity {
+    private EditText guess;
+    private int score;
+    private TextView scoreText, messageText;
+    private int randNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_layout);
+        messageText = (TextView) findViewById(R.id.textMessage);
+        guess = (EditText) findViewById(R.id.numGuess);
+
+    }
+
+    private void generateNumbers() {
+        Random rand = new Random();
+        randNum = rand.nextInt(100);
+    }
+
+    private String check() {
+        int number = Integer.parseInt(String.valueOf(guess.getText()));
+        String notify = "Correct!";
+        if(randNum>number) {
+            notify = "Guess too low, try again";
+        }
+        else if(randNum<number) {
+            notify = "Guess too high, try again";
+        }
+        return notify;
+    }
+
+   public void btnClicked(View view) {
+        messageText.setText(this.check());
+   }
 }
--- app/src/main/java/nz/ac/aut/dms/android/MainActivity.java
+++ app/src/main/java/nz/ac/aut/dms/android/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText range;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +19,6 @@ public class MainActivity extends AppCompatActivity {
     public void btnPlayOnClick(View view) {
         startActivity(new Intent(this, GameActivity.class));
     }
+
+
 }
